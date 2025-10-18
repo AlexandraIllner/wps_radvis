2d063a4c373a8fac21d2803737cbe1755cce38c9
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2025-10-18 15:22:50 +0200

    chore: add backend project structure and base packages

---
+++ backend/radvis-mock-backend/src/main/java/de/htw/radvis_mock_backend/RadvisMockBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htw.radvis_mock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadvisMockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadvisMockBackendApplication.class, args);
+	}
+
+}
