5e57d4148a8cf172ac682386254bce708d5986f5
Mira.Khreis <mira@example.com>
2025-10-20 00:03:54 +0200

    Add Angular Material dropdown and theme configuration

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+
   protected readonly title = signal('frontend');
+
+  selectedCategory: string | null = null;
+
+  categories: string[] = [
+    'Schlagloch',
+    'Schlechter Straßenbelag',
+    'Bewuchs',
+    'Fehlende Beschilderung',
+    'Falsche Beschilderung',
+    'Poller/Hindernis',
+    'Unklare Markierung',
+  ];
 }
