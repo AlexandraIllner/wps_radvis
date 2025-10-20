325969c25fc9184281141c48b4ff6757e365a4c6
Yadigar Camci <yadigar.camci@student.htw-berlin.de>
2025-10-20 19:21:10 +0200

    feat(frontend): implement basic global error interceptor and register it in app config

---
+++ frontend/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error) => {
+      console.log('Ein Fehler wurde erkannt!');
+      console.log('Status:', error.status);
+      console.log('Nachricht:', error.message);
+
+      alert('Es ist ein Fehler aufgetreten.');
+
+      return throwError(() => error);
+    })
+  );
+};
