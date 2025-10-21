381463ea2efc5b8305019c47b78244caffd4707a
Mira.Khreis <mira@example.com>
2025-10-21 13:42:09 +0200

    Add Absenden button below description field

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-root',
