7910ad2ed43d51df457452bd6974a0b9ced47ad3
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2025-10-23 20:37:50 +0200

    chore(issue): Include 'POLLER_HINDERNS, 'UNKLARE_MARKIERUNG'and

--- backend/src/main/java/de/htw/radvis/domain/Issue.java
+++ backend/src/main/java/de/htw/radvis/domain/Issue.java
@@ -5,7 +5,10 @@ public enum Issue {
     SCHLECHTER_STRASSENBELAG("Schlechter Straßenbelag"),
     BEWUCHS("Bewuchs"),
     FEHLENDE_BESCHILDERUNG("Fehlende Beschilderung"),
-    FALSCHE_BESCHILDERUNG("Falsche Beschilderung");
+    FALSCHE_BESCHILDERUNG("Falsche Beschilderung"),
+    POLLER_HINDERNIS("Poller/Hindernis"),
+    UNKLARE_MARKIERUNG("Unklare Markierung"),
+    UNEBENHEITEN_BODENWELLEN("Unebenheiten/Bodenwellen");
 
     private final String label;
 
