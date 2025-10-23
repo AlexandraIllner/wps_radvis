76e59e42f59b38c1fa0b5dae4800fd8757ffd387
Yadigar Camci <yadigar.camci@student.htw-berlin.de>
2025-10-23 14:18:52 +0200

    feat: add golbalServicefile with api.service Class for endpoint communication

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {errorInterceptor} from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
