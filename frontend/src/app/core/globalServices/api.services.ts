eac95961c45b42267dbd074e4a380331e57b3b0d
Yadigar Camci <yadigar.camci@student.htw-berlin.de>
2025-10-24 07:33:20 +0200

    feat: add API methods in api.service to complete backend communication

---
+++ frontend/src/app/core/globalServices/api.services.ts
@@ -0,0 +1,32 @@
+/**
+ * ===========================================================================
+ * ApiService - HTTP Communication Service
+ * ============================================================================
+**/
+
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../enviroments/enviroment';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+  })
+
+export class ApiServices{
+  private readonly baseUrl=environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+
+  //GET-Abfrage: Issue
+  getIssue(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/api/issues`)
+  }
+
+  createReport(data:any): Observable<any>{
+    return this.http.post(`${this.baseUrl}/api/report`,data)
+  }
+
+}
