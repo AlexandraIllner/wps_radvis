1489d303e177f392ae3e04a0d2bd4dca5db0c1e6
Yadigar <YadiSphere@github.com>
2025-11-06 21:04:06 +0100

    feat: add loading spinner and snackbar warning when upload limit (max 3 files) is exceeded; automatically keeps the first 3 photos

--- frontend/src/app/photo-upload/photo-upload.ts
+++ frontend/src/app/photo-upload/photo-upload.ts
@@ -2,13 +2,15 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 @Component({
   selector: 'app-photo-upload',
   imports: [
     MatButton,
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './photo-upload.html',
   styleUrl: './photo-upload.css'
@@ -24,29 +26,27 @@ export class PhotoUpload {
   isUploadDisabled = false;
 
 
+   @Output() photosSelected = new EventEmitter<File[]>()
+
   constructor(private snackBar: MatSnackBar) {
 
   }
 
+  onUploadClick(galleryInput: HTMLInputElement): void {
+    // Wenn gerade kein Upload läuft
+    if (!this.isLoading) {
+      this.isLoading = true; // Ladezustand aktivieren
 
-  private isValidFile(file: File): boolean {
-    //Konvertierung des gesamten Strings in Kleinbuchstaben-> Einheitliche Prüfung möglich
-    const fileName = file.name.toLowerCase();
-    const maxSize = 5 * 1024 * 1024; //Max. 5 MB
+      // Dateiauswahl öffnen
+      galleryInput.click();
 
-    //1. Prüft die Dateiendung
-    const hasValidExtension = fileName.endsWith(".jpg") || fileName.endsWith(".png");
-
-    //2. Prüft die Dateigröße
-    const hasValidSize = file.size <= maxSize;
-
-    //Beide müssen TRUE sein
-    return hasValidExtension && hasValidSize;
-
+      // Optional: Ladezustand simulieren (z. B. 2 Sekunden)
+      setTimeout(() => {
+        this.isLoading = false; // Ladezustand beenden
+      }, 2000);
+    }
   }
 
-
-  @Output() photosSelected = new EventEmitter<File[]>()
 
 
   onFilesSelected(event: Event): void {
@@ -62,6 +62,16 @@ export class PhotoUpload {
           this.invalidCount++  // wenn ungültig → zählen
         }
       }
+      // Falls der Nutzer mehr als 3 auswählt → Hinweis anzeigen
+      if (input.files.length > 3) {
+        this.snackBar.open(
+          'Maximal 3 Dateien erlaubt. Nur die ersten 3 wurden übernommen.',
+          'OK',
+          { duration: 3000 }
+        );
+      }
+
+
       // Warnung anzeigen, falls ungültige Dateien vorhanden sind
       if (this.invalidCount > 0) {
         this.snackBar.open(`${this.invalidCount} Datei(en) ungültig. Es sind nur JPG/PNG Datein und max. 5MB erlaubt.`,
@@ -93,6 +103,23 @@ export class PhotoUpload {
     this.previewUrls.splice(index, 1);
     this.photosSelected.emit(this.selectedFiles);
   }
+
+  private isValidFile(file: File): boolean {
+    //Konvertierung des gesamten Strings in Kleinbuchstaben-> Einheitliche Prüfung möglich
+    const fileName = file.name.toLowerCase();
+    const maxSize = 5 * 1024 * 1024; //Max. 5 MB
+
+    //1. Prüft die Dateiendung
+    const hasValidExtension = fileName.endsWith(".jpg") || fileName.endsWith(".png");
+
+    //2. Prüft die Dateigröße
+    const hasValidSize = file.size <= maxSize;
+
+    //Beide müssen TRUE sein
+    return hasValidExtension && hasValidSize;
+
+  }
+
 
 
 }
