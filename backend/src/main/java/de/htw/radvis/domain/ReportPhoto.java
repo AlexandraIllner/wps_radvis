01e34497ebac7ffe8cd81e21e68b849ee3783cbc
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2025-11-08 21:07:29 +0100

    feat(domain): add ReportPhoto entity and establish one-to-many relation with Report

---
+++ backend/src/main/java/de/htw/radvis/domain/ReportPhoto.java
@@ -0,0 +1,47 @@
+package de.htw.radvis.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "report_photo")
+public class ReportPhoto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB", nullable = false)
+    private byte[] data;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+
+    // ------------- Getter & Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+
+    public void setReport(Report report) {
+        this.report = report;
+    }
+}
