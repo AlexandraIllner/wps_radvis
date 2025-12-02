5a7c5510ac82902ffce91675ca0a206a8ac53b1a
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2025-12-02 16:32:22 +0100

    test: add unit test for valid file selection

--- frontend/src/app/photo-upload/photo-upload.spec.ts
+++ frontend/src/app/photo-upload/photo-upload.spec.ts
@@ -1,25 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoUpload } from './photo-upload';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 describe('PhotoUpload', () => {
   let component: PhotoUpload;
+  let fixture: ComponentFixture<PhotoUpload>;
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
 
   beforeEach(async () => {
-    const snackBarMock = { open: () => {} };
+    snackBarSpy = jasmine.createSpyObj('MatSnackBar', ['open']);
 
     await TestBed.configureTestingModule({
-      imports: [PhotoUpload],
+      imports: [PhotoUpload, NoopAnimationsModule],
       providers: [
-        { provide: MatSnackBar, useValue: snackBarMock },
-      ],
+        { provide: MatSnackBar, useValue: snackBarSpy }
+      ]
     }).compileComponents();
 
-    const fixture = TestBed.createComponent(PhotoUpload);
+    fixture = TestBed.createComponent(PhotoUpload);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('soll erstellt werden', () => {
     expect(component).toBeTruthy();
   });
+
+  it('soll gültige Datei erstellen und emitten', () => {
+    spyOn(component.photosSelected, 'emit');
+
+    const file = new File([''], 'valid.jpg', { type: 'image/jpeg'});
+    const event = { target: {files: [file], value: ''}} as unknown as Event;
+
+    component.onFilesSelected(event);
+
+    expect(component.photosSelected.length).toBe(1);
+    expect(component.photosSelected.emit).toHaveBeenCalledWith([file]);
+    expect(snackBarSpy.open).not.toHaveBeenCalled();
+  })
 });
