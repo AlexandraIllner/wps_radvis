0cc45a4d224331ae437fa43c0f414becba8d9156
Angela <a.barzaeva@gmail.com>
2025-12-09 20:17:26 +0100

    feat(map): add hint box for location selection

--- frontend/src/app/karte/karte.ts
+++ frontend/src/app/karte/karte.ts
@@ -5,12 +5,14 @@ import 'leaflet.locatecontrol';
 import { NgxLeafletLocateModule } from '@runette/ngx-leaflet-locate';
 import { MatButton } from '@angular/material/button';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-karte',
   templateUrl: './karte.html',
   styleUrls: ['./karte.css'],
-  imports: [LeafletDirective, NgxLeafletLocateModule, MatButton, MatGridList, MatGridTile],
+  imports: [LeafletDirective, NgxLeafletLocateModule, MatButton, MatGridList,MatIconModule, MatGridTile],
   standalone: true,
 })
 export class Karte {
