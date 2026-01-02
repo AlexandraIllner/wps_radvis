4f27da3e860f9e51f29a162f3419a934fc81ffc2
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2026-01-02 18:15:37 +0100

    fix(ci): adjust Karma config for CI test execution

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -2,6 +2,7 @@ import generated from '@angular-devkit/build-angular/plugins/karma';
 
 module.exports = function (config) {
   const isCI = !!process.env.CI;
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -19,26 +20,18 @@ module.exports = function (config) {
       suppressAll: true,
     },
     reporters: ['progress', 'kjhtml', 'junit'],
-
     junitReporter: {
       outputDir: 'test-results',
       outputFile: 'junit.xml',
       useBrowserName: false,
     },
 
-    /*
-    auskommentierte Teile für Linux-Systeme,
-    die ChromeHeadless nicht out of the box unterstützen
-    LINUX:
-    browsers: ['google-chrome-stable --headless', 'ChromeHeadlessCI'],
-    */
     browsers: ['ChromeHeadless'],
     singleRun: isCI,
     restartOnFileChange: !isCI,
+
     customLaunchers: {
       ChromeHeadlessCI: {
-        // LINUX
-        // base: 'google-chrome-stable --headless',
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu'],
       },
